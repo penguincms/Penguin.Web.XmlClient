8513bbf9878397269945b3bdec38b9f8c6a49136
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-10 12:13:34 -0700

    Autocommit
    Autocommit

--- XmlClient.cs
+++ XmlClient.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using Penguin.Reflection.Serialization.XML;
 using System.Net;
-using System.Text;
-using Penguin.Reflection.Serialization.XML;
 
-namespace Penguin.Web
+namespace Penguin.Web.XmlClient
 {
     /// <summary>
     /// WebClient with options to upload/download objects by converting to/from XML, for XML endpoints. Use "UploadXml" and "DownloadXml"
@@ -13,25 +9,36 @@ namespace Penguin.Web
     public class XmlClient : WebClient
     {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
         public XmlClient()
         {
-           
         }
 
         public T DownloadXml<T>(string url) where T : class
         {
-             return new XMLSerializer().DeserializeObject<T>(this.DownloadString(url));
+            return new XMLSerializer().DeserializeObject<T>(DownloadString(url));
         }
 
         public T UploadXml<T>(string url, object toUpload) where T : class
         {
-            return new XMLSerializer().DeserializeObject<T>(this.UploadString(url, new XMLSerializer().SerializeObject(toUpload)));
+            return new XMLSerializer().DeserializeObject<T>(UploadString(url, new XMLSerializer().SerializeObject(toUpload)));
         }
 
         public string UploadXml(string url, object toUpload)
         {
-            return this.UploadString(url, new XMLSerializer().SerializeObject(toUpload));
+            return UploadString(url, new XMLSerializer().SerializeObject(toUpload));
         }
+
+        public T DownloadXml<T>(System.Uri url) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T UploadXml<T>(System.Uri url, object toUpload) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 }
